48bc4a54cfb56a7e4814ce765c250571ab0a2357
adixek <kaz14adi@gmail.com>
2026-02-17 18:10:56 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -61,7 +61,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_USE_DATE_BINARY=1
     	export FOX_DELETE_AROMAFM=1
     	export TARGET_DEVICE_ALT="pipa"
-        export FOX_VENDOR_BOOT_RECOVERY=1
+   #     export FOX_VENDOR_BOOT_RECOVERY=1
 	# instruct magiskboot v24+ to always patch the vbmeta header when patching the recovery/boot image; do *not* remove!
         export FOX_PATCH_VBMETA_FLAG="1"
 
